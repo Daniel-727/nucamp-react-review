5dc1a5df73dbbc8b1fffb59cb74d685634659e13
Daniel Li <zaoster777@gmail.com>
2022-01-31 15:42:54 -0600

    after react components 2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import './App.css';
 import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Directory from './components/DirectoryComponent';
+import {CAMPSITES} from './shared/campsites'; //CAMPSITES is wrapped in curly braces because there is no default export from campsites. CAMPSITES IS A NAMED EXPORT
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color='primary'>
-          <div className='container'>
-              <NavbarBrand href='/'>Nucamp</NavbarBrand>
-          </div>
-      </Navbar>
-      <Directory/>
-    </div>
-  );
-}
+class App extends Component {
+    constructor(props){
+        super(props); //must have super(props) as first line of constructor -required by React
+        this.state = {
+            campsites: CAMPSITES
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+              <Navbar dark color='primary'>
+                  <div className='container'>
+                      <NavbarBrand href='/'>Nucamp</NavbarBrand>
+                  </div>
+              </Navbar>
+              <Directory campsites={this.state.campsites}/>
+            </div>
+          );
+        }
+    }
 
 export default App;
